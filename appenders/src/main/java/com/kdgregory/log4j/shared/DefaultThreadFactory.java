6abf6bf83ad0086c9bec63e3b0719b50013ea062
Keith Gregory <contact@kdgregory.com>
2017-06-24 14:36:18 -0400

    name each logger thread

--- appenders/src/main/java/com/kdgregory/log4j/shared/DefaultThreadFactory.java
+++ appenders/src/main/java/com/kdgregory/log4j/shared/DefaultThreadFactory.java
@@ -1,6 +1,7 @@
 // Copyright (c) Keith D Gregory, all rights reserved
 package com.kdgregory.log4j.shared;
 
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  *  The default {@link ThreadFactory} for most appenders: creates a normal-priority
@@ -8,10 +9,13 @@ package com.kdgregory.log4j.shared;
  */
 public class DefaultThreadFactory implements ThreadFactory
 {
+    private AtomicInteger threadNumber = new AtomicInteger();
+    
     @Override
     public void startLoggingThread(LogWriter writer)
     {
         Thread writerThread = new Thread(writer);
+        writerThread.setName("log4j-writer-" + threadNumber.getAndIncrement());
         writerThread.setPriority(Thread.NORM_PRIORITY);
         writerThread.start();
     }

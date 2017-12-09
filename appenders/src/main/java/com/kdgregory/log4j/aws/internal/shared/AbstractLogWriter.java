b88d758461afaecec5aa7c7965f5672c0e62fa33
Keith Gregory <contact@kdgregory.com>
2017-12-09 08:36:21 -0500

    new initialization code was allowing interrupts by integration tests

--- appenders/src/main/java/com/kdgregory/log4j/aws/internal/shared/AbstractLogWriter.java
+++ appenders/src/main/java/com/kdgregory/log4j/aws/internal/shared/AbstractLogWriter.java
@@ -127,6 +127,12 @@ implements LogWriter
             return;
         }
 
+        // this is set after initialization because the integration tests were telling
+        // the writer to shut down while it was still initializing; in the real world
+        // that should never happen without the appender being shut down as well
+
+        dispatchThread = Thread.currentThread();
+
         // this reports that initialization succeeded
 
         initializationMessage = "";
@@ -264,7 +270,6 @@ implements LogWriter
     {
         try
         {
-            dispatchThread = Thread.currentThread();
             createAWSClient();
             return ensureDestinationAvailable();
         }

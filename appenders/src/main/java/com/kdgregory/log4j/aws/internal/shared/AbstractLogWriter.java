kdgregory/log4j-aws-appenders

commit 61fe372a716be902f667775be3b26cc31ea05493

KinesisLogWriter: retry DescribeStream if rate-limited (along with similar code in integration test)

appenders/src/main/java/com/kdgregory/log4j/aws/internal/shared/AbstractLogWriter.java
@@ -127,12 +127,6 @@ implements LogWriter
             return;
         }
 
-        // this is set after initialization because the integration tests were telling
-        // the writer to shut down while it was still initializing; in the real world
-        // that should never happen without the appender being shut down as well
-
-        dispatchThread = Thread.currentThread();
-
         // this reports that initialization succeeded
 
         initializationMessage = "";
@@ -270,6 +264,7 @@ implements LogWriter
     {
         try
         {
+            dispatchThread = Thread.currentThread();
             createAWSClient();
             return ensureDestinationAvailable();
         }

appenders/src/main/java/com/kdgregory/log4j/aws/internal/kinesis/KinesisLogWriter.java
@@ -20,6 +20,12 @@ import com.kdgregory.log4j.aws.internal.shared.Utils;
 public class KinesisLogWriter
 extends AbstractLogWriter
 {
+    // this controls the number of times that we'll accept rate limiting on describe
+    private final static int DESCRIBE_TRIES = 300;
+
+    // and the number of milliseconds to sleep between tries
+    private final static int DESCRIBE_SLEEP = 100;
+
     // this controls the number of tries that we wait for a stream to become active
     private final static int STREAM_ACTIVE_TRIES = 240;
 
@@ -107,7 +113,6 @@ extends AbstractLogWriter
     }
 
 
-
     @Override
     protected boolean withinServiceLimits(int batchBytes, int numMessages)
     {
@@ -172,20 +177,30 @@ extends AbstractLogWriter
 
 
     /**
-     *  Returns current stream status, null if the stream doesn't exist.
+     *  Returns current stream status, null if the stream doesn't exist. Will
+     *  internally handle rate-limit exceptions, retrying every 100 milliseconds
+     *  and eventually timing out after 30 seconds with an IllegalStateException.
      */
     private String getStreamStatus()
     {
-        try
+        for (int ii = 0 ; ii < DESCRIBE_TRIES ; ii++)
         {
-            DescribeStreamRequest request = new DescribeStreamRequest().withStreamName(config.streamName);
-            DescribeStreamResult response = client.describeStream(request);
-            return response.getStreamDescription().getStreamStatus();
+            try
+            {
+                DescribeStreamRequest request = new DescribeStreamRequest().withStreamName(config.streamName);
+                DescribeStreamResult response = client.describeStream(request);
+                return response.getStreamDescription().getStreamStatus();
+            }
+            catch (ResourceNotFoundException ex)
+            {
+                return null;
+            }
+            catch (LimitExceededException ex)
+            {
+                Utils.sleepQuietly(DESCRIBE_SLEEP);
+            }
         }
-        catch (ResourceNotFoundException ex)
-        {
-            return null;
-        }
+        throw new IllegalStateException("unable to describe stream after 30 seconds");
     }
 
 

appenders/src/test/java/com/kdgregory/log4j/aws/TestKinesisAppender.java
@@ -267,6 +267,39 @@ public class TestKinesisAppender
 
 
     @Test
+    public void testRateLimitedDescribe() throws Exception
+    {
+        initialize("TestKinesisAppender/testRateLimitedDescribe.properties");
+
+        MockKinesisClient mockClient = new MockKinesisClient()
+        {
+            @Override
+            protected DescribeStreamResult describeStream(DescribeStreamRequest request)
+            {
+                if (describeStreamInvocationCount < 3)
+                    throw new LimitExceededException("uh oh!");
+                else
+                    return super.describeStream(request);
+            }
+        };
+
+        appender.setThreadFactory(new DefaultThreadFactory());
+        appender.setWriterFactory(mockClient.newWriterFactory());
+
+        logger.debug("example message");
+        mockClient.allowWriterThread();
+
+        // even with exceptions we should be able to write our message
+
+        assertEquals("describeStream: invocation count",        3,          mockClient.describeStreamInvocationCount);
+        assertEquals("describeStream: stream name",             "argle",    mockClient.describeStreamStreamName);
+        assertEquals("createStream: invocation count",          0,          mockClient.createStreamInvocationCount);
+        assertEquals("putRecords: invocation count",            1,          mockClient.putRecordsInvocationCount);
+        assertEquals("putRecords: source record count",         1,          mockClient.putRecordsSourceRecords.size());
+    }
+
+
+    @Test
     public void testInitializationErrorHandling() throws Exception
     {
         initialize("TestKinesisAppender/testInitializationErrorHandling.properties");

appenders/src/test/resources/TestKinesisAppender/testRateLimitedDescribe.properties
@@ -1,0 +1,12 @@
+# config for mock-object testing; note low batch size and delay
+
+log4j.rootLogger=DEBUG, default
+
+log4j.appender.default=com.kdgregory.log4j.testhelpers.aws.kinesis.TestableKinesisAppender
+log4j.appender.default.layout=org.apache.log4j.PatternLayout
+log4j.appender.default.layout.ConversionPattern=%m%n
+
+log4j.appender.default.streamName=argle
+log4j.appender.default.partitionKey=bargle
+log4j.appender.default.batchSize=20
+log4j.appender.default.batchDelay=100

tests/src/test/java/com/kdgregory/log4j/aws/KinesisAppenderIntegrationTest.java
@@ -163,8 +163,8 @@ public class KinesisAppenderIntegrationTest
 //----------------------------------------------------------------------------
 
     /**
-     *  Returns the stream description, null if the stream doesn't exist.
-     *  @return
+     *  Returns the stream description, null for any exception (which will
+     *  eventually time out if continued).
      */
     private StreamDescription describeStream()
     {
@@ -174,7 +174,7 @@ public class KinesisAppenderIntegrationTest
             DescribeStreamResult describeReponse  = client.describeStream(describeRequest);
             return describeReponse.getStreamDescription();
         }
-        catch (ResourceNotFoundException ignored)
+        catch (Exception ignored)
         {
             return null;
         }

commit f911450c1a2acfcfab8d87a86f914a791bb35c2e

61fe372a accidentally reverted b88d7584

appenders/src/main/java/com/kdgregory/log4j/aws/internal/shared/AbstractLogWriter.java
@@ -125,7 +125,13 @@ implements LogWriter
                 LogLog.error("initialization failed: " + initializationMessage);
 
             return;
-        }
+        }        
+        
+        // this is set after initialization because the integration tests were telling
+        // the writer to shut down while it was still initializing; in the real world
+        // that should never happen without the appender being shut down as well
+
+        dispatchThread = Thread.currentThread();
 
         // this reports that initialization succeeded
 
@@ -264,7 +270,6 @@ implements LogWriter
     {
         try
         {
-            dispatchThread = Thread.currentThread();
             createAWSClient();
             return ensureDestinationAvailable();
         }
f92c5357bb4031209e49679801d164963f974863
Keith Gregory <contact@kdgregory.com>
2017-12-30 15:55:21 -0500

    apply Apache license boilerplate to all Java source

--- appenders/src/main/java/com/kdgregory/log4j/aws/internal/shared/DefaultThreadFactory.java
+++ appenders/src/main/java/com/kdgregory/log4j/aws/internal/shared/DefaultThreadFactory.java
@@ -1,8 +1,22 @@
-// Copyright (c) Keith D Gregory, all rights reserved
+// Copyright (c) Keith D Gregory
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.kdgregory.log4j.aws.internal.shared;
 
 import java.lang.Thread.UncaughtExceptionHandler;
 import java.util.concurrent.atomic.AtomicInteger;
+
 
 /**
  *  The default {@link ThreadFactory} for most appenders: creates a normal-priority

afb0f0299445fb689e2fd4d7d98aada8bf9e2161
Keith Gregory <contact@kdgregory.com>
2018-07-17 20:10:33 -0400

    add property

--- appenders/src/test/java/com/kdgregory/log4j/aws/TestKinesisAppender.java
+++ appenders/src/test/java/com/kdgregory/log4j/aws/TestKinesisAppender.java
@@ -142,13 +142,14 @@ public class TestKinesisAppender
 
         assertEquals("stream name",         "argle-{bargle}",                   appender.getStreamName());
         assertEquals("partition key",       "foo-{date}",                       appender.getPartitionKey());
-        assertEquals("shard count",         7,                                  appender.getShardCount());
-        assertEquals("retention period",    48,                                 appender.getRetentionPeriod());
         assertEquals("max delay",           1234L,                              appender.getBatchDelay());
         assertEquals("discard threshold",   54321,                              appender.getDiscardThreshold());
         assertEquals("discard action",      "newest",                           appender.getDiscardAction());
         assertEquals("client factory",      "com.example.Foo.bar",              appender.getClientFactory());
         assertEquals("client endpoint",     "kinesis.us-west-1.amazonaws.com",  appender.getClientEndpoint());
+        assertTrue("autoCreate",                                                appender.isAutoCreate());
+        assertEquals("shard count",         7,                                  appender.getShardCount());
+        assertEquals("retention period",    48,                                 appender.getRetentionPeriod());
     }
 
 
@@ -159,13 +160,14 @@ public class TestKinesisAppender
 
         // don't test stream name because there's no default
         assertEquals("partition key",       "{startupTimestamp}",               appender.getPartitionKey());
-        assertEquals("shard count",         1,                                  appender.getShardCount());
-        assertEquals("retention period",    24,                                 appender.getRetentionPeriod());
         assertEquals("max delay",           2000L,                              appender.getBatchDelay());
         assertEquals("discard threshold",   10000,                              appender.getDiscardThreshold());
         assertEquals("discard action",      "oldest",                           appender.getDiscardAction());
         assertEquals("client factory",      null,                               appender.getClientFactory());
         assertEquals("client endpoint",     null,                               appender.getClientEndpoint());
+        assertFalse("autoCreate",                                                appender.isAutoCreate());
+        assertEquals("shard count",         1,                                  appender.getShardCount());
+        assertEquals("retention period",    24,                                 appender.getRetentionPeriod());
     }
 
 

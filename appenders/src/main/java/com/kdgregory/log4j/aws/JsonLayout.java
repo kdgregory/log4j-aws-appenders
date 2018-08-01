c2d65f61a03d0a667e5b31eb55c81a8aba995366
Keith Gregory <contact@kdgregory.com>
2018-07-31 20:31:30 -0400

    implement

--- appenders/src/main/java/com/kdgregory/log4j/aws/JsonLayout.java
+++ appenders/src/main/java/com/kdgregory/log4j/aws/JsonLayout.java
@@ -3,9 +3,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 // http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -106,10 +106,22 @@ extends Layout
 //  Configuration
 //----------------------------------------------------------------------------
 
+    private boolean appendNewlines;
     private boolean enableLocation;
     private boolean enableHostname;
     private boolean enableInstanceId;
     private String rawTags;
+
+
+    public void setAppendNewlines(boolean value)
+    {
+        appendNewlines = value;
+    }
+
+    public boolean getAppendNewlines()
+    {
+        return appendNewlines;
+    }
 
 
     public void setEnableLocation(boolean value)
@@ -240,7 +252,13 @@ extends Layout
             map.put("locationInfo", location);
         }
 
-        return converterTL.get().convert(map);
+        String json = converterTL.get().convert(map);
+        if (getAppendNewlines())
+        {
+            json += "\n";
+        }
+
+        return json;
     }
 
 
